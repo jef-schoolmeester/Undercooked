d84540834dc7de1c2f8b8a4445cea5153a88bc3d
PierreA <pierre.abeille@g-mail.fr>
2020-11-14 10:56:55 +0100

    3eme Test Commit

---
+++ src/reciepe/State.java
@@ -0,0 +1,13 @@
+package reciepe;
+
+/*
+ * Enum State permettant de définir l'état d'un ingrédient ou d'un plat
+ * 
+ * @author Pierre Abeille
+ */
+
+public enum State {
+	RAW,
+	COOKED,
+	TRASH
+}
