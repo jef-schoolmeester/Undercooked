519d62408d871e438932154bd1bd073108207664
PierreA <pierre.abeille@g-mail.fr>
2020-11-26 15:06:59 +0100

    added Maven

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,11 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        URL url = new File("src/main/java/sample/sample.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1100, 800));
         primaryStage.show();
