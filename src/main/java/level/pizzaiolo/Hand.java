3722b8bb36e6a7c90a17978e3e502403c79b2285
PierreA <pierre.abeille@g-mail.fr>
2020-11-27 11:01:42 +0100

    [FEAT] added Methods to Piazzaiolo and Hand -getters and setters -useTool -takeIngredientFromTool -isHandFull -isIngredient -isDish

--- src/main/java/level/pizzaiolo/Hand.java
+++ src/main/java/level/pizzaiolo/Hand.java
@@ -6,10 +6,37 @@ import level.recipe.Ingredient;
 public class Hand {
       private Ingredient ingredient;
       private Dish dish;
-      private Boolean isHandFull;
 
       public Hand() {
           this.ingredient = null;
           this.dish = null;
       }
+
+      public boolean isHandFull() {
+          return this.dish != null || this.ingredient != null;
+      }
+
+      public boolean isIngredient() {
+          return this.ingredient != null && this.dish == null;
+      }
+
+    public boolean isDish() {
+        return this.dish != null && this.ingredient == null;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    protected void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    protected void setDish(Dish dish) {
+        this.dish = dish;
+    }
 }
--- src/main/java/level/pizzaiolo/Pizzaiolo.java
+++ src/main/java/level/pizzaiolo/Pizzaiolo.java
@@ -1,9 +1,13 @@
 package level.pizzaiolo;
 //package level;
 
+import level.recipe.Ingredient;
+import level.tools.Tool;
+
 public class Pizzaiolo {
     private int posX;
     private int posY;
+    private Hand hand;
 
     //Pour l'instant le constructeur est à zéro, mais j'imagine que le pizzaiolo spawnera au milieu de la map, j'ai besoin de table.
     public Pizzaiolo() {
@@ -14,5 +18,31 @@ public class Pizzaiolo {
         this.posY = Table.getnbRow()/2;
 
  */
+    }
+
+    public boolean useTool(Tool tool){
+        if (this.hand.isHandFull() && tool.isUsable(this.posX, this.posY)){
+            tool.addIngredient(this.hand.getIngredient());
+            return tool.use();
+        }else {
+            return false;
+        }
+
+    }
+
+    public void takeIngredientFromTool(Tool tool){
+        Ingredient newIngredient;
+        if(tool.isUsable(this.posX, this.posY) && !this.hand.isHandFull()){
+           newIngredient = tool.takeIngredient();
+           this.hand.setIngredient(newIngredient);
+        }
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
     }
 }
