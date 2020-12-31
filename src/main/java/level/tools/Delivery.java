bbaae3adcce88e6c5681c85d16d951d5b025e426
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2020-12-31 01:21:11 +0100

    [FEAT] Delivery & oven

--- src/main/java/level/tools/Delivery.java
+++ src/main/java/level/tools/Delivery.java
@@ -1,8 +1,32 @@
 package level.tools;
 
+import level.recipe.Dish;
+
+import java.util.ArrayList;
+
 public class Delivery extends DishTool{
+
+    private ArrayList<Dish> preparedDishes;
+
     public Delivery(int posX, int posY) {
         super(posX, posY);
+        this.preparedDishes = new ArrayList<>();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.preparedDishes.isEmpty();
+    }
+
+    @Override
+    public boolean addDish(Dish dish) {
+        this.preparedDishes.add(dish);
+        return true;
+    }
+
+    @Override
+    public Dish takeDish() {
+        return null;
     }
 
     public String toString() {
--- src/main/java/level/tools/Oven.java
+++ src/main/java/level/tools/Oven.java
@@ -1,23 +1,48 @@
 package level.tools;
 
+import level.recipe.Dish;
 import level.recipe.StateDish;
 import level.recipe.StateIngredient;
+import level.threads.OvenThread;
 
 public class Oven extends DishTool {
 
-    public Oven(int posX, int posY) {
+    private OvenThread ovenThread;
+
+    public Oven(int posX, int posY) throws InterruptedException {
         super(posX, posY);
+        this.ovenThread = new OvenThread(this);
+        this.ovenThread.start();
     }
 
-    public boolean use() {
-        return true;
-        //A FAIRE
+    @Override
+    public Dish takeDish() {
+        Dish currentDish = this.dish;
+        this.dish = null;
+        this.ovenThread.toggle();
+        return currentDish;
     }
-    
+
+    //Function name
+    public void burn() {
+        switch (this.dish.getStateDish()) {
+            case RAW -> this.dish.setStateDish(StateDish.COOKED);
+            default -> this.dish.setStateDish(StateDish.TRASH);
+        }
+    }
+
+    @Override
+    public boolean use() {
+        ovenThread.toggle();
+        return true;
+    }
+
+    @Override
     public String toString() {
         return "oven";
     }
 
+    @Override
     public String imgPath() {
         return "/IB/tools/oven.png";
     }
