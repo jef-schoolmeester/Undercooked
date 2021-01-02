2bf7b1218fa434bbc96c6ca9f1b70232dcd192b6
Jef-io <73157784+Jef-io@users.noreply.github.com>
2021-01-02 10:48:49 +0100

    Update User.java

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -21,7 +21,7 @@ public class User {
                 "mongodb://udUser:EFBDrRdeE8QAG1lU@cluster0-shard-00-00.wcngd.mongodb.net:27017,cluster0-shard-00-01.wcngd.mongodb.net:27017,cluster0-shard-00-02.wcngd.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-ofie07-shard-0&authSource=admin&retryWrites=true&w=majority");
         MongoClient mongoClient = new MongoClient(uri);
         this.database = mongoClient.getDatabase("UndercookedDB");
-        this.userName = "null";
+        this.userName = "not connected";
     }
 
     public MongoCursor getUsers() {
