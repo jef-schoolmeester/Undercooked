708d231dd0de474df4123d14bcafeee397b103f3
Pierre Abeille <pierre.abeille@g-mail.fr>
2021-01-02 15:31:38 +0100

    [FEAT] first commit

--- src/main/java/sample/levelSelectController.java
+++ src/main/java/sample/levelSelectController.java
@@ -12,6 +12,11 @@ import java.net.URL;
 public class levelSelectController {
     public Button goBackButton;
 
+    /**
+     *
+     * @param mouseEvent
+     * @throws IOException
+     */
     public void changeScene(MouseEvent mouseEvent) throws IOException {
         URL url = new File("src/main/java/sample/mainMenu.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
