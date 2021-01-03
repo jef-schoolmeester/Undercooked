601e882b85ca6e89c7f70151af9033de086f7a17
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-01-03 18:03:51 +0100

    [FEAT] Ingredient tools doc

--- src/main/java/level/tools/Knife.java
+++ src/main/java/level/tools/Knife.java
@@ -1,14 +1,38 @@
 package level.tools;
 
+/**
+ * Knife class
+ * Used to transform most of the ingredients
+ * @author Jef
+ * @since 1.0
+ * @version 1.0
+ */
 public class Knife extends IngredientTool {
+
+    /**
+     * Class constructor
+     * @param posX X axis position to be set
+     * @param posY Y axis position to be set
+     * @since 1.0
+     */
     public Knife(int posX, int posY) {
         super(posX, posY);
     }
 
+    /**
+     *
+     * @return the class name as string
+     * @since 1.0
+     */
     public String toString() {
         return "knife";
     }
 
+    /**
+     *
+     * @return an image path to the knife
+     * @since 1.0
+     */
     public String getImagePath() {
         return "/IB/tools/knife.png";
     }
--- src/main/java/level/tools/TrashCan.java
+++ src/main/java/level/tools/TrashCan.java
@@ -2,29 +2,60 @@ package level.tools;
 
 import level.recipe.Ingredient;
 
+/**
+ * Trashcan class
+ * Any ingredients or dishes can be throw in this class
+ * @author Jef
+ * @since 1.0
+ * @version 1.0
+ */
 public class TrashCan extends IngredientTool {
+
+    /**
+     * Class constructor
+     * @param posX X axis position to be set
+     * @param posY Y axis position to be set
+     * @since 1.0
+     */
     public TrashCan(int posX, int posY) {
         super(posX, posY);
     }
 
-
+    /**
+     *
+     * @return false because a trashcan cannot be used
+     * @since 1.0
+     */
     @Override
     public boolean use() {
         return false;
     }
 
+    /**
+     *
+     * @param ingredient the ingredient to be thrown in the trashcan
+     * @return always true, there is no space limit to the trashcan
+     * @since 1.0
+     */
     public boolean addIngredient(Ingredient ingredient) {
         return true;
     }
 
-    public Ingredient takeIngredient() {
-        return null;
-    }
 
+    /**
+     *
+     * @return the class name in a string
+     * @since 1.0
+     */
     public String toString() {
         return "trashCan";
     }
 
+    /**
+     *
+     * @return the path to the trashCan png
+     * @since 1.0
+     */
     public String getImagePath() {
         return "/IB/tools/trashCan.png";
     }
