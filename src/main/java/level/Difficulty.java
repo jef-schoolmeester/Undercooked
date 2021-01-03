aed53f780af56153ec920116f4053a1c2ee5e501
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-01-03 14:39:40 +0100

    [FEAT] Difficulty + Tile + voidTile + Interface  tool docs

--- src/main/java/level/Difficulty.java
+++ src/main/java/level/Difficulty.java
@@ -1,5 +1,12 @@
 package level;
 
+/**
+ * Enum that defines the difficulty of a level
+ *
+ * @author Jef
+ * @since 1.0
+ * @version 1.0
+ */
 public enum Difficulty {
     EASY,
     NORMAL,
--- src/main/java/level/tools/InterfaceTool.java
+++ src/main/java/level/tools/InterfaceTool.java
@@ -1,9 +1,17 @@
 package level.tools;
 
+/**
+ * A interface with the basic methods required for tools
+ * @see IngredientTool
+ * @see DishTool
+ *
+ * @author Jef
+ * @since 1.0
+ * @version 1.0
+ */
 public interface InterfaceTool {
     boolean isUsable(int posX, int posY);
     boolean isEmpty();
     String getImagePath();
     String toString();
-
 }
